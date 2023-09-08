b54af1cae19a26d46f4ddb6c2509f6462114c0a8
Nadezhda Pertyakova <n.pertyakova@mail.ru>
2023-09-08 13:08:32 +0300

    first commit

---
+++ src/Form.js
@@ -0,0 +1,160 @@
+/* import {useFormik} from 'formik'; */
+import { Formik, Form, Field, ErrorMessage, useField } from 'formik';
+import * as Yup from 'yup';
+
+/* const validate = values => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'Обязательное поле для заполнения!';
+    } else if (values.name.length < 2) {
+        errors.name = 'Минимум 2 символа для заполнения!';
+    }
+    if (!values.email) {
+        errors.email = 'Обязательное поле для заполнения!';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Некорректный адрес';
+      }
+    return errors;
+}
+ */
+const MyTextInput = ({label, ...props}) => {
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.name}>{label}</label>
+            <input {...field} {...props}/>
+                {meta.touched && meta.error ? (
+                    <div className='error'>{meta.error}</div>
+                ) : null}
+        </>        
+    )
+}
+
+const MyCheckbox = ({ children, ...props }) => {
+    const [field, meta] = useField({ ...props, type: 'checkbox' });
+    return (
+      <>
+        <label className="checkbox">
+            <input type="checkbox" {...field} {...props} />
+            {children}
+        </label>
+        {meta.touched && meta.error ? (
+          <div className="error">{meta.error}</div>
+        ) : null}
+      </>
+    );
+  };
+
+const CustomForm = () => {
+    /* const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            amount: '',
+            currency: '',
+            text: '',
+            terms: false
+        }, */
+        /* validate */
+       /*  validationSchema: Yup.object({
+            name: Yup.string()
+                    .min(2, 'Минимум 2 символа для заполнения!')
+                    .required('Обязательное поле для заполнения!'),
+            email: Yup.string()
+                        .email('Некорректный адрес')   
+                        .required('Обязательное поле для заполнения!'),
+            amount: Yup.number()
+                    .min(5, 'Не менее 5!')
+                    .required('Обязательное поле для заполнения!'),            
+            currency: Yup.string()
+                        .required('Выберите валюту!'),
+            text: Yup.string()
+                    .min(10, 'Не менее 10 символов!'),
+            terms: Yup.boolean()
+                        .required('Необходимо согласие!')
+                        .oneOf([true], 'Необходимо согласие!')                    
+
+        }),
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    }) */
+    return (
+        <Formik
+            initialValues={{
+                name: '',
+                email: '',
+                amount: '',
+                currency: '',
+                text: '',
+                terms: false
+            }}
+            validationSchema={
+                Yup.object({
+                    name: Yup.string()
+                            .min(2, 'Минимум 2 символа для заполнения!')
+                            .required('Обязательное поле для заполнения!'),
+                    email: Yup.string()
+                                .email('Некорректный адрес')   
+                                .required('Обязательное поле для заполнения!'),
+                    amount: Yup.number()
+                            .min(5, 'Не менее 5!')
+                            .required('Обязательное поле для заполнения!'),            
+                    currency: Yup.string()
+                                .required('Выберите валюту!'),
+                    text: Yup.string()
+                            .min(10, 'Не менее 10 символов!'),
+                    terms: Yup.boolean()
+                                .required('Необходимо согласие!')
+                                .oneOf([true], 'Необходимо согласие!')                
+                })}
+                onSubmit={values => console.log(JSON.stringify(values, null, 2))}>
+            <Form className="form">
+                <h2>Отправить пожертвование</h2>
+                <MyTextInput
+                    label="Ваше имя"
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="off"
+                />
+                <MyTextInput
+                    label="Ваша почта"
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="off"
+                />               
+                <label htmlFor="amount">Количество</label>
+                <Field
+                    id="amount"
+                    name="amount"
+                    type="number"                    
+                />
+                <ErrorMessage className='error' name='amount' component='div'/>
+                <label htmlFor="currency">Валюта</label>
+                <Field
+                    id="currency"
+                    name="currency"
+                    as='select'>
+                        <option value="">Выберите валюту</option>
+                        <option value="USD">USD</option>
+                        <option value="UAH">UAH</option>
+                        <option value="RUB">RUB</option>
+                </Field>
+                <ErrorMessage className='error' name='currency' component='div'/>
+                <label htmlFor="text">Ваше сообщение</label>
+                <Field 
+                    id="text"
+                    name="text"
+                    as='textarea'/>
+                <ErrorMessage className='error' name='text' component='div'/>
+                <MyCheckbox name='terms'>
+                    Соглашаетесь с политикой конфиденциальности?
+                </MyCheckbox>
+                <button type="submit">Отправить</button>
+            </Form>
+        </Formik>
+        
+    )
+}
+
+export default CustomForm;
